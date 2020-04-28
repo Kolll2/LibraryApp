418f91ba24c4465c5543bd6fc3f792b480421764
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-04-28 15:02:23 +0400

    add saveBook pane interface

--- src/main/java/com/kolll/FXMLDocumentController.java
+++ src/main/java/com/kolll/FXMLDocumentController.java
@@ -1,4 +1,39 @@
 package com.kolll;
 
-public class FXMLDocumentController {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private JFXTextField title;
+    @FXML
+    private JFXTextField id;
+    @FXML
+    private JFXTextField author;
+    @FXML
+    private JFXTextField publicher;
+    @FXML
+    private JFXButton saveButton;
+    @FXML
+    private JFXButton cancelButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void saveBook(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void cancelSave(ActionEvent actionEvent) {
+    }
 }
